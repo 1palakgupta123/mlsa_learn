e52c4a79d160f1c0f04e756c9f07cb39d2af291e
1palakgupta123 <116502152+1palakgupta123@users.noreply.github.com>
2024-02-05 23:33:07 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -35,25 +35,9 @@ const projectList = [
     description:
       "Effortlessly transform text into spoken words with our seamless and versatile text-to-speech converter.",
     url: " https://1palakgupta123.github.io/texttospeech/",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  }
+ 
+  
 ];
 
 const Portfolio = () => {
